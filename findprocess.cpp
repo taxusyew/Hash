519cb4fb8c2b12baa2c8f799f507ec76753a4979
taxusyew <taxusyew@gmail.com>
2014-07-09 17:08:05 +0800

    windows32下遍历进程及其模块

---
+++ findprocess.cpp
@@ -0,0 +1,62 @@
+#include "stdio.h"
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+
+void main()
+{
+	HANDLE hprocSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32  pe;
+	MODULEENTRY32 me32;
+	//hprocSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	BOOL existProc = Process32First(hprocSnap, &pe);
+	TCHAR*   shortpath = new TCHAR[256];
+
+	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+	
+	me32.dwSize = sizeof(MODULEENTRY32);
+	hModuleSnap = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, pe.th32ProcessID );
+	int n = 0;
+	
+	 do{ 
+		n++;
+
+		printf("PID: %d\n", pe.th32ProcessID);
+
+		hModuleSnap = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, pe.th32ProcessID );
+		if( hModuleSnap == INVALID_HANDLE_VALUE )
+		{
+			printf("can not find module");
+			continue;
+		}
+
+		if (!Module32First(hModuleSnap, &me32))
+		{
+			printf("can not find module");
+			continue;
+		}
+
+		while (Module32Next(hModuleSnap, &me32))
+		{
+			printf("%s\n", me32.szExePath);
+		}
+		
+		//Module32First(hModule, &minfo);  
+		//GetShortPathName(minfo.szExePath,shortpath,256); 
+		
+		//printf("%d", pe.th32ProcessID);
+		//printf("%s --- %s \n",pe.szExeFile,shortpath); 
+
+	 }while (Process32Next(hprocSnap,&pe));
+	
+
+	CloseHandle(hprocSnap);
+	CloseHandle(hModuleSnap);
+
+	printf("Total Process: %d\n", n);
+
+	system("pause");
+
+}
