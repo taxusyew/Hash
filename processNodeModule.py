bbc5744d2bb96d1c431f7882432de2d06a96e601
taxus <taxusyew@gmail.com>
2015-11-10 18:00:14 +0800

    添加注释、取消多余代码

--- processNodeModule.py
+++ processNodeModule.py
@@ -8,31 +8,25 @@ def renameDir(curDir):
         print os.listdir(curDir)
         index = 0
         newDirs = []
-        newDir  = ''
+        newDir = ''
+        # 切换为当前文件夹
         os.chdir(curDir)
-        # os.rename('D:\code\fountain-new\b', 'D:\code\fountain-new\a0')
-        for single_dir in curDirs:
-                if os.path.isdir(single_dir):
-                        origin_dir = os.path.join(os.path.dirname(curDir), single_dir) 
-                        newDir = 'a'+str(index)
-                        new_dir = os.path.join(os.path.dirname(curDir), newDir)
-                        # print '1', origin_dir
-                        # print '2', new_dir
-                        newDirs.append(newDir)
-                        # os.rename(origin_dir, new_dir)
-                        os.rename(single_dir, newDir)
-                        index+=1
-                        
-        print newDirs
-        for new_single_dir in newDirs:
-                # for new_single_dir in curDirs:
-                # child_dir = os.path.join(os.path.dirname(curDir), new_single_dir)
-                child_dir = os.path.join(curDir, new_single_dir)
-                # print '3',child_dir
-                # os.chdir(curDir)
-                renameDir(child_dir)
-                # print os.listdir(child_dir)
 
+        # 遍历当前目录下的所有文件
+        for single_dir in curDirs:
+                # 只处理文件夹
+                if os.path.isdir(single_dir):
+                        newDir = 'a'+str(index)
+                        # python list 没有 push，用 append 代替
+                        newDirs.append(newDir)
+                        os.rename(single_dir, newDir)
+                        index += 1
+
+        # 递归处理子文件夹
+        for new_single_dir in newDirs:
+                # 如果当前
+                child_dir = os.path.join(curDir, new_single_dir)
+                renameDir(child_dir)
 
         return
 
